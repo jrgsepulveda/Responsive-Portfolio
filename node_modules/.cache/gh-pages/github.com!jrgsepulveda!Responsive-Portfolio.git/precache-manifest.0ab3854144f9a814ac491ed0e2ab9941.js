7260e8921bdd96d828befa1e67e2f981ebf5c39f
Jorge Sepulveda <jrg.sepulveda@gmail.com>
2020-11-23 20:01:42 -0500

    turn into reactive portfolio

---
+++ node_modules/.cache/gh-pages/github.com!jrgsepulveda!Responsive-Portfolio.git/precache-manifest.0ab3854144f9a814ac491ed0e2ab9941.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0198df63490f8f1742072e504e5dd012",
+    "url": "/Responsive-Portfolio/index.html"
+  },
+  {
+    "revision": "702ae73da5fbfde90021",
+    "url": "/Responsive-Portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f29ece731f21b2276a11",
+    "url": "/Responsive-Portfolio/static/js/2.e4e08dad.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/Responsive-Portfolio/static/js/2.e4e08dad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "702ae73da5fbfde90021",
+    "url": "/Responsive-Portfolio/static/js/main.0ffa4281.chunk.js"
+  },
+  {
+    "revision": "3c419356e3581e9d7e71",
+    "url": "/Responsive-Portfolio/static/js/runtime-main.bf37ba44.js"
+  }
+]);
